30a7214bc79acb6d925d753f8c6490feb0cf4124
ohhyungsuh <ohhyungsuh@gmail.com>
2025-01-09 13:32:59 +0900

    git init

---
+++ src/main/java/com/example/to_do_server/ToDoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.to_do_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/to_do_server/ToDoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
