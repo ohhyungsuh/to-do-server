d93f7dd6ec7326b9340f8dba5fb2c244171230c4
ohhyungsuh <ohhyungsuh@gmail.com>
2025-01-14 20:54:16 +0900

    feat: 일반 사용자가 요청하는 API 구현
    - 그룹 가입 요청
    - 그룹 가입 요청 삭제
    - 그룹 내 인원 조회
    - 그룹 나가기

--- src/main/java/com/example/to_do_server/user_group/presentation/UserGroupController.java
+++ src/main/java/com/example/to_do_server/user_group/presentation/UserGroupController.java
@@ -1,28 +1,57 @@
 package com.example.to_do_server.user_group.presentation;
 
+import com.example.to_do_server.global.response.BaseResponse;
+import com.example.to_do_server.session.SessionUtils;
+import com.example.to_do_server.user_group.domain.dto.UserInfoDto;
 import com.example.to_do_server.user_group.service.UserGroupService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping(value = "/api/v1/user-group")
 public class UserGroupController {
 
     private final UserGroupService userGroupService;
 
-    // todo 그룹 가입 요청
+    // todo 알림 테이블 만들어서 실시간 알람 기능 만들어보기
+    // 그룹 가입 요청
+    @PostMapping(value = "/{groupId}")
+    public ResponseEntity<BaseResponse<?>> sendJoin(@PathVariable Long groupId, HttpServletRequest request) {
+        Long userId = SessionUtils.getUserIdBySession(request);
+        userGroupService.sendJoin(userId, groupId);
+        return ResponseEntity.ok(BaseResponse.success("그룹 가입 요청을 보냈습니다. 승인까지 기다려주세요."));
+    }
 
-    // todo 그룹 가입 요청 삭제
+    // 그룹 가입 요청 삭제
+    @DeleteMapping(value = "/{groupId}/remove")
+    public ResponseEntity<BaseResponse<?>> deleteJoin(@PathVariable Long groupId, HttpServletRequest request) {
+        Long userId = SessionUtils.getUserIdBySession(request);
+        userGroupService.deleteJoin(userId, groupId);
+        return ResponseEntity.ok(BaseResponse.success("그룹 가입 요청을 삭제했습니다."));
+    }
 
-    // todo 내가 참여 중인, 참여 대기 중인 그룹 조회(API 나눠도 됨)
+    // 그룹 내 인원 조회
+    @GetMapping(value = "/{groupId}/users")
+    public ResponseEntity<BaseResponse<List<UserInfoDto>>> getUserInfoInGroup(@PathVariable Long groupId, HttpServletRequest request) {
+        SessionUtils.getUserIdBySession(request);
+        List<UserInfoDto> userInfos = userGroupService.getUserInfoInGroup(groupId);
+        return ResponseEntity.ok(BaseResponse.success("그룹 인원들을 조회했습니다.", userInfos));
+    }
 
-    // todo 그룹 내 인원 조회
-
-    // todo 그룹 나가기
+    // 그룹 나가기
+    @DeleteMapping(value = "/{groupId}/leave")
+    public ResponseEntity<BaseResponse<?>> leaveGroup(@PathVariable Long groupId, HttpServletRequest request) {
+        Long userId = SessionUtils.getUserIdBySession(request);
+        userGroupService.deleteJoin(userId, groupId);
+        return ResponseEntity.ok(BaseResponse.success("그룹을 나갔습니다."));
+    }
 
 
 
--- src/main/java/com/example/to_do_server/user_group/service/UserGroupService.java
+++ src/main/java/com/example/to_do_server/user_group/service/UserGroupService.java
@@ -1,10 +1,22 @@
 package com.example.to_do_server.user_group.service;
 
+import com.example.to_do_server.global.exception.GlobalException;
+import com.example.to_do_server.global.response.ErrorCode;
+import com.example.to_do_server.group.domain.Group;
 import com.example.to_do_server.group.domain.repository.GroupRepository;
+import com.example.to_do_server.user.domain.User;
 import com.example.to_do_server.user.domain.repository.UserRepository;
+import com.example.to_do_server.user_group.domain.Status;
+import com.example.to_do_server.user_group.domain.UserGroup;
+import com.example.to_do_server.user_group.domain.dto.UserInfoDto;
 import com.example.to_do_server.user_group.domain.repository.UserGroupRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,16 +26,55 @@ public class UserGroupService {
     private final GroupRepository groupRepository;
     private final UserGroupRepository userGroupRepository;
 
-    // todo 그룹 가입 요청
+    // 그룹 가입 요청
+    @Transactional
+    public void sendJoin(Long userId, Long groupId) {
+        Optional<UserGroup> findUserGroup = userGroupRepository.findByUserIdAndGroupId(userId, groupId);
 
-    // todo 그룹 가입 요청 삭제
+        if (findUserGroup.isPresent()) {
+            throw GlobalException.from(ErrorCode.EXIST_USER_GROUP);
+        }
 
-    // todo 내가 참여 중인, 참여 대기 중인 그룹 조회(API 나눠도 됨)
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> GlobalException.from(ErrorCode.INVALID_SESSION));
 
-    // todo 그룹 내 인원 조회
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(() -> GlobalException.from(ErrorCode.NOT_EXIST_GROUP));
 
-    // todo 그룹 나가기
+        UserGroup userGroup = UserGroup.builder()
+                .user(user)
+                .group(group)
+                .status(Status.PENDING)
+                .build();
 
+        userGroupRepository.save(userGroup);
+    }
+
+    // 그룹 가입 요청 삭제
+    @Transactional
+    public void deleteJoin(Long userId, Long groupId) {
+        UserGroup userGroup = getUserGroupByUserIdAndGroupId(userId, groupId);
+
+        userGroupRepository.deleteById(userGroup.getId());
+    }
+
+    // 그룹 내 인원 조회
+    public List<UserInfoDto> getUserInfoInGroup(Long groupId) {
+        List<UserGroup> userGroups = userGroupRepository.findByGroupId(groupId);
+
+        return userGroups.stream()
+                .filter(userGroup -> userGroup.getStatus().equals(Status.JOIN))
+                .map(userGroup -> new UserInfoDto(userGroup.getUser()))
+                .collect(Collectors.toList());
+    }
+
+    // 그룹 나가기
+    @Transactional
+    public void leaveGroup(Long userId, Long groupId) {
+        UserGroup userGroup = getUserGroupByUserIdAndGroupId(userId, groupId);
+
+        userGroupRepository.deleteById(userGroup.getId());
+    }
 
 
     // todo 그룹 가입 요청 수락
@@ -33,4 +84,10 @@ public class UserGroupService {
     // todo 그룹 가입 요청 조회
 
     // todo 그룹 내 인원 추방
+
+
+    private UserGroup getUserGroupByUserIdAndGroupId(Long userId, Long groupId) {
+        return userGroupRepository.findByUserIdAndGroupId(userId, groupId)
+                .orElseThrow(() -> GlobalException.from(ErrorCode.INVALID_REQUEST));
+    }
 }
